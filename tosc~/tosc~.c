0e37e247cfbc25edb8d1a9fc8213eee8f07b6694
spacenoidsound <102999284+spacenoidsound@users.noreply.github.com>
2024-07-01 08:31:56 +0100

    corrected wrong folder copy

--- tosc~/tosc~.c
+++ tosc~/tosc~.c
@@ -1,7 +1,7 @@
 
  
-
- //tosc~ a wavetable oscillator that can be controlled via two inlets for frequency and harmonics.
+//tosc~ a wave table oscillator with inlets for frequency and harmonics
+ 
  
  #include "ext.h"            // standard Max include, always required (except in Jitter)
  #include "ext_obex.h"        // required for "new" style objects
@@ -21,19 +21,22 @@
 
 
 //struct for the oscillator implementation with pointer to processing which will implement the wave_this_osc synth processing
- typedef struct _base_osc_ {
+ typedef struct _t_osc_ {
+     t_pxobject        ob;
      double h;          //harmonics
      double amp;   //amplitude
      double tab[TSIZE]; //table for the wave synth with a set size.
      double sig[BSIZE];  //array for the signal
      double sr;         //sample rate
      double frq;        //frequency (adjustable via inlet 1)
-      double ph;   //phase
-     double *(*process)(struct _base_osc_ *, double, double); //processing method allocated with parameter space for tsize and bsize
- } Base_osc;
+     double ph;   //phase
+     double tsize;
+     double bsize;
+     double *(*process)(struct _t_osc_ *, double, double); //processing method allocated with parameter space for tsize and bsize
+ } t_tosc;
 
 
-double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
+double *wave_this_osc(t_tosc *syn, double tsize, double bsize) {
     for(int i = 0; i < tsize; i++) {
         int ind = (int)(syn->ph * tsize); //method takes in table, phase, and frequency dereferenced from inputed oscillator via syn
         if (ind < 0) {
@@ -43,7 +46,7 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
         }
         syn->sig[i] = 0.5 * syn->tab[ind];
         syn->ph += syn->frq / syn->sr;
-        while (syn->ph >= 1.0) {   //ensuring wrapping the phase.
+        while (syn->ph >= 1.0) {   //ensuring wrapping the phase.  Initially had table size but had to resort to 0 to 1.
             syn->ph -= 1.0;
             {
         while (syn->ph < 0) {
@@ -55,21 +58,7 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
     return syn->sig;
 }
     
-    
-    // struct to represent the object's state
-    typedef struct _tosc {
-        t_pxobject        ob;            // the object itself (t_pxobject in MSP instead of t_object)
-        Base_osc       osc;
-        double         *sig;
-        double         *t; //for the wave table
-        double         tsize;  //table size
-        double             bsize;  //buffer size
-        double            amp;     // storing the amplitude here
-        double              f;  //frequency
-        double             h; //harmonics
-    } t_tosc;
-    
-    
+
     // global class pointer variable
     static t_class *tosc_class = NULL;
     
@@ -82,8 +71,6 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
     void simplemsp_int(t_tosc *x, long n);
     void simplemsp_dsp64(t_tosc *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags);
     void simplemsp_perform64(t_tosc *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam);
-    void tosc_setting_freq(t_symbol *x);
-    void tosc_setting_harm(t_symbol *x);
 
     
     //***********************************************************************************************
@@ -113,28 +100,25 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
             
             dsp_setup((t_pxobject *)x, 2);    // MSP inlets: arg is # of inlets
             outlet_new((t_object *)x, "signal");         // signal outlet (note "signal" rather than NULL)
-            
-            x->h = 10;  //initializing with basic parameters
-            x->osc.h = 10;  //harmonics
+            //initializing with basic parameters
+
+            x->h = 10;              
             x->tsize = TSIZE;
             x->bsize = BSIZE;
             
-            x->osc.ph = 0.0; //setting the inital phase here which can be manipulated with 0.0 to 1.0 input inlet
-            x->f = 440.0; //default frequency
-            x->osc.frq = 440;
+            x->ph = 0.0; //setting the inital phase here which can be manipulated with 0.0 to 1.0 input inlet
+            x->frq = 440.; //default frequency
             x->amp = 0.5;
-            x->osc.sr = sys_getsr();  //getting sample rate from the system
+            x->sr = sys_getsr();  //getting sample rate from the system
             
-            x->osc.process = wave_this_osc; //process using the wave_synth function
+            x->process = wave_this_osc; //process using the wave_synth function
             
             //ability to write in arguments for frequency and harmonics
             if (argc > 0 && atom_gettype(argv) == A_FLOAT) {
-                x->osc.frq = atom_gettype(argv);
-                x->f = atom_gettype(argv);
+                x->frq = atom_gettype(argv);
             }
             
             if (argc > 1 && atom_gettype(argv + 1) == A_FLOAT) {
-                x->osc.h = atom_gettype(argv + 1);
                 x->h = atom_gettype(argv + 1);
             }
 
@@ -144,17 +128,17 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
             //Fourier Table with harmonics
 
             for (int i = 0; i < x->tsize; i++) {
-                x->osc.tab[i] = 0.0;
+                x->tab[i] = 0.0;
                 
                 //loop cycles through harmonics to create waveform
                 for (int j = 1; j <= x->h; j++) { //j is the harmonic (harm)
                     
-                    double time = (double)i/x->osc.sr;
+                    double time = (double)i/x->sr;
     
                     //checks string input from message inlet and iterates through the harmonics depending on the value,
-                    x->osc.tab[i] += sin(TWOPI * x->f * j * time)/j; //if left as "none" then loops through all the harmonics
+                    x->tab[i] += sin(TWOPI * x->frq * j * time)/j; //if left as "none" then loops through all the harmonics
                 }
-                    x->osc.tab[i] *= x->amp;
+                    x->tab[i] *= x->amp;
                 
             }
             return (x);
@@ -189,15 +173,12 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
     {
         //post("Received the frequency: %.5f", f);
         
-        x->osc.frq = f;
-        x->f = f;
+        x->frq = f;
     }
     
     //handles in freq input is a int rather than float
     void simplemsp_int(t_tosc *x, long n) {
         
-        //x->osc.h = h;
-        //x->h = h;
         simplemsp_float(x, (double) n);
     }
     
@@ -207,7 +188,7 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
     void simplemsp_dsp64(t_tosc *x, t_object *dsp64, short *count, double samplerate, long maxvectorsize, long flags)
     {
         
-        x->osc.sr = samplerate;
+        x->sr = samplerate;
         
         object_method(dsp64, gensym("dsp_add64"), x, simplemsp_perform64, 0, NULL);
     }
@@ -217,7 +198,7 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
     // this is the 64-bit perform method audio vectors
     void simplemsp_perform64(t_tosc *x, t_object *dsp64, double **ins, long numins, double **outs, long numouts, long sampleframes, long flags, void *userparam)
     {
-        t_double *inF = ins[0];        // frequency
+        t_double *inF = ins[0];        // frequency/
         t_double *inH = ins[1];      //harmonics
         t_double *outL = outs[0];    // we get audio for each outlet of the object from the **outs argument
         
@@ -226,19 +207,19 @@ double *wave_this_osc(Base_osc *syn, double tsize, double bsize) {
         // this perform method simply copies the input to the output, offsetting the value
         while (n--) {
             
-            double* out_s = x->osc.process(&(x->osc), x->tsize, x->bsize);
+            double* out_s = x->process(x, x->tsize, x->bsize);
             
             for(int i = 0; i < n; i++){
                 double f = inF[i]; //putting the frequency in f from the input inlet
                 if (f != 0.0) {
-                    x->osc.frq = f;
+                    x->frq = f;
                 }
             }
             
             for(int i = 0; i < n; i++){
-                double harm = inH[i]; //putting the harmonics in h from the input inlet
+                double harm = inH[i]; //putting the harmonic in h from the input inlet
                 if (harm != 0.0) {
-                    x->osc.h = harm;
+                    x->h = harm;
                 }
             }
             
